bed1831038a1fd65c107bf7878eecde50333ac1a
anna <anna@localhost>
2026-02-27 09:12:01 +0900

    課題

---
+++ src/test01/cytech01.java
@@ -0,0 +1,23 @@
+package test01;
+
+public class cytech01 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		int num;
+		num = 100;
+		
+		String text;
+		text = "あいうえお";
+		
+		boolean flag = true;
+		
+		
+		System.out.println(num);
+		System.out.println(text);
+		System.out.println(flag);
+
+	}
+
+}
